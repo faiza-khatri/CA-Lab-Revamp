536730e2be0f5e35b4060bea210ad65635c3306a
faiza-khatri <faizasalman.k45@gmail.com>
2025-10-07 11:35:50 +0500

    removed instmem from writable logic

--- lab8/src/writeDeMux.v
+++ lab8/src/writeDeMux.v
@@ -4,31 +4,26 @@ module writeDeMux(
     input writeEnable,
 
     input selDataMem,
-    input selInstMem,
     input selLed,
     
     output reg dataMemWriteEnable,
-    output reg instMemWriteEnable,
     output reg ledWriteEnable
     );
     
     always @(*) begin
         if (selDataMem) begin
             dataMemWriteEnable <= writeEnable;
-            instMemWriteEnable <= 0;
             ledWriteEnable <= 0;
-        end
-        else if (selInstMem) begin
-            dataMemWriteEnable <= 0;
-            instMemWriteEnable <= writeEnable;
-            ledWriteEnable <= 0;
-        end
-        if (selLed) begin
-            dataMemWriteEnable <= 0;
-            instMemWriteEnable <= 0;
-            ledWriteEnable <= writeEnable;
         end
 
+        if (selLed) begin
+            dataMemWriteEnable <= 0;
+            ledWriteEnable <= writeEnable;
+        end
+        else begin
+            dataMemWriteEnable <= 0;
+            ledWriteEnable <= 0;
+        end
     end
     
 endmodule
